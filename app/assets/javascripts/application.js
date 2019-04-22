04cf33f19fa1b289a45fe197e827928ca2ae6485
Nicolas Firavitoba <nicofira.dev@gmail.com>
2019-04-22 11:48:01 -0500

    JS fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 //= require turbolinks
 //= require_tree .
 
-$(document).on('turbolinks:load',() => {
+$(document).on('turbolinks:load',function() {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
